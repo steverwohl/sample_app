e2b65ad25a389792d3a32760fde386d16313c316
Steven Wohl <steverwohl@gmail.com>
2014-04-03 10:02:17 +0000

    Finished the static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
   end
+
 
   describe "Help page" do
 
@@ -16,6 +22,11 @@ describe "Static pages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
   end
 
   describe "About page" do
@@ -23,5 +34,10 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
+    
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end
   end
 end
